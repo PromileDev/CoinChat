01675dadc10f0a99ad8c71cbd0d8d70af9dc8208
Luis <abaca@inscastellet.cat>
2024-10-15 13:07:48 +0200

    por defecto /start language en es

--- cogs/Language.py
+++ cogs/Language.py
@@ -6,6 +6,11 @@ from cogs import ManageBD
 
 async def selectLanguage(update: Update, context):
     user_id = update.message.from_user.id
+    # Obtener el idioma del usuario
+    language = ManageBD.getLanguage(user_id)
+    # Si no se encuentra ningún idioma, usar 'es' por defecto
+    if language is None:
+        language = 'es'
     # Crear botones en fila (Reply Keyboard)
     keyboard = [
         [KeyboardButton("Español 🇪🇸"), KeyboardButton("English 🇬🇧")]
@@ -13,4 +18,4 @@ async def selectLanguage(update: Update, context):
     # Envuelve los botones en un teclado que aparece encima del campo de texto
     reply_markup = ReplyKeyboardMarkup(keyboard, resize_keyboard=True, one_time_keyboard=True)
     # Envía un mensaje con el teclado de respuesta
-    await update.message.reply_text(ManageBD.getPrompt(ManageBD.getLanguage(user_id), "seleectLen_msg"), reply_markup=reply_markup)
+    await update.message.reply_text(ManageBD.getPrompt(language, "seleectLen_msg"), reply_markup=reply_markup)
